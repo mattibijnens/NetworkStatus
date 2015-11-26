829b8a9cb01e7f025d9118bc76d33357c6f896c8
sanderblaadjes <matti.bijnens@hotmail.com>
2015-11-26 19:58:50 +0100

    second commit

--- NetworkStatus/NetworkStatus/Form1.cs
+++ NetworkStatus/NetworkStatus/Form1.cs
@@ -16,5 +16,10 @@ namespace NetworkStatus
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
