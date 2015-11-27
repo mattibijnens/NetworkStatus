b480914a5374b943a66b00a78f4548ba03e51410
sanderblaadjes <matti.bijnens@hotmail.com>
2015-11-27 17:58:16 +0100

    Additions

--- NetworkStatus/NetworkStatus/Form1.cs
+++ NetworkStatus/NetworkStatus/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +14,27 @@ namespace NetworkStatus
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
+           { 
+
+        
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("test");
+            Speedtest.folderName.Replace(@"//", @"/");
+            if (!File.Exists(Speedtest.folderName)) {
+                Directory.CreateDirectory(Speedtest.folderName);
+            }
         }
     }
 }
